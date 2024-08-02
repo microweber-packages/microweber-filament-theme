c764ec4108c49bb186ef7491f2ac4ec8b825f4d8
Bozhidar <bobi@microweber.com>
2024-08-02 14:19:38 +0300

    update

--- src/MicroweberFilamentThemeServiceProvider.php
+++ src/MicroweberFilamentThemeServiceProvider.php
@@ -2,7 +2,20 @@
 
 namespace MicroweberPackages\MicroweberFilamentTheme;
 
-class MicroweberFilamentThemeServiceProvider extends \Illuminate\Support\ServiceProvider
-{
 
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class MicroweberFilamentThemeServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('microweber-filament-theme');
+    }
+
+    public function packageBooted(): void
+    {
+        MicroweberFilamentTheme::configure();
+    }
 }
