9b9bae7948ff26e2f72d3dcb8bf95cf34fe17fda
Bozhidar <bobi@microweber.com>
2024-08-02 14:52:33 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,24 @@ const preset = require('./vendor/filament/filament/tailwind.config.preset')
 
 module.exports = {
     presets: [preset],
-    content: [
-        './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
+    plugins: [
+
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/container-queries'),
+
     ],
+
+    content: [
+        './app/Filament/Admin/**/*.php',
+        './resources/views/filament/admin/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+        './src/MicroweberPackages/**/*.blade.php',
+        './src/MicroweberPackages/**/resources/views/**/*.blade.php',
+        './vendor/jaocero/radio-deck/resources/views/**/*.blade.php',
+        './userfiles/modules/**/*.blade.php',
+        './userfiles/modules/**/src/resources/views/**/*.blade.php',
+    ]
 }
